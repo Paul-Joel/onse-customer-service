36ad7f66737d556988014ed1739267b0e25a8c12
Paul-Joel <paul.joel@ons.gov.uk>
2019-02-13 15:58:09 +0000

    blank line

--- features/steps/update_customer.py
+++ features/steps/update_customer.py
@@ -1,8 +1,5 @@
 from behave import when, then, given
 
-from customer_service.model.customer import Customer
-
-
 @when('I update customer "{id}" to "{name}"')
 def update_customer(context, id, name):
     (first_name, surname) = name.split(' ', 2)
@@ -12,4 +9,3 @@ def update_customer(context, id, name):
 
     assert response.status_code == 201, response.status_code
     context.customer_id = response.get_json()['customerId']
-
